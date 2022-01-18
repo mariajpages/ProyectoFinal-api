06db7fef73506eb9c64081b6027535640a615771
mariajpages <mariaj.pages@gmail.com>
2022-01-17 21:53:20 -0300

    mongo

--- db/mongoose.js
+++ db/mongoose.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 const dbUser = process.env.dbUser;
 const dbPass = process.env.dbPass;
 
-const url = `mongodb+srv://${dbUser}:${dbPass}@cluster0.yrdcz.mongodb.net/paginaweb?retryWrites=true&w=majority`
+const url = `mongodb+srv://${dbUser}:${dbPass}@cluster0.yrdcz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
 
 
 mongoose.connect(url, {
